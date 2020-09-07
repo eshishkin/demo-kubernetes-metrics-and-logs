63dba88f68b2daf13677e58e08b8c0b4ea323656
Eugene Shishkin <shishkin.john@gmail.com>
2020-09-07 11:34:05 +0400

    #1: skeleton for sender service
    Signed-off-by: Eugene Shishkin <shishkin.john@gmail.com>

---
+++ sender-service/src/test/java/org/eshishkin/edu/sender/SenderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eshishkin.edu.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
